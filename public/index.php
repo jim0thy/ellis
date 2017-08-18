118dc3a4f7a7d2e6d663d26d9847268b301cbd60
Jim Gillett <jim.gillett@clifton-asset.co.uk>
2017-08-18 16:44:03 +0100

    Check if env variables are available before loading

--- public/index.php
+++ public/index.php
@@ -5,8 +5,10 @@ use Dotenv\Dotenv;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$dotEnv = new Dotenv(__DIR__.'/../');
-$dotEnv->load();
+if (!isset($_ENV['SERIAL_NO'])) {
+    $dotEnv = new Dotenv(__DIR__ . '/../');
+    $dotEnv->load();
+}
 
 $serialNo = $_ENV['SERIAL_NO'];
 $userId = $_ENV['USER_ID'];
